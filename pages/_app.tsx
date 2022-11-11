debf64d68df72e8109fd67e0731daf4499154c29
FelipLTavares <felipelimatavares96@gmail.com>
2022-11-11 16:37:18 -0300

    Config changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import MainMenu from "../components/UI/mainMenu/MainMenu";
+import { MainMenu } from "../components/UI/mainMenu/MainMenu";
 import { GlobalStyles } from "../styles/GlobalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import FeaturedProdSection from "../components/client/Home/featuredProductsSection/FeaturedProdSection";
-import Outdoor from "../components/client/Home/outdoor/Outdoor";
+import { HighlightedFeatures } from "../components/client/Home/featuredHighlights/HighlightedFeatures";
+import { FeaturedProducts } from "../components/client/Home/featuredProducts/FeaturedProdSection";
+import { Outdoor } from "../components/client/Home/outdoor/Outdoor";
 
 export default function Home() {
   return (
     <>
       <Outdoor />
-      <FeaturedProdSection></FeaturedProdSection>
+      <FeaturedProducts />
+      <HighlightedFeatures />
     </>
   );
 }
