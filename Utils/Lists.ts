9ec83f47e472c1818b46b9e5ed7680fa7b7c4069
FelipLTavares <felipelimatavares96@gmail.com>
2022-11-11 16:37:59 -0300

    Some data to show

---
+++ Utils/Lists.ts
@@ -0,0 +1,26 @@
+export const Products = [
+  {
+    id: "1",
+    type: "Calça",
+    name: "Calça Preta",
+    value: "R$ 100,00",
+    image:
+      "https://res.cloudinary.com/dh60zlsir/image/upload/v1650666030/loja/gawcptakrv0dfobjtd3p.jpg",
+  },
+  {
+    id: "2",
+    type: "Calça",
+    name: "Calça Preta",
+    value: "R$ 100,00",
+    image:
+      "https://res.cloudinary.com/dh60zlsir/image/upload/v1650666030/loja/gawcptakrv0dfobjtd3p.jpg",
+  },
+  {
+    id: "3",
+    type: "Calça",
+    name: "Calça Preta",
+    value: "R$ 100,00",
+    image:
+      "https://res.cloudinary.com/dh60zlsir/image/upload/v1650666030/loja/gawcptakrv0dfobjtd3p.jpg",
+  },
+];
