5a83d6e452088f1a86011c2175a8c5c620426708
FelipLTavares <felipelimatavares96@gmail.com>
2022-11-11 16:33:55 -0300

    Path changed

--- components/client/Home/featuredProductsSection/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from "@stitches/react";
-
-export const Featured = styled("div", {
-  width: "100%",
-  marginTop: 64,
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr 1fr",
-  justifyItems: "center",
-});
